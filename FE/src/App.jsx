518aaf7e3cacdd5b44188ba30acc1b8d33a53c68
ilayoron <ilayoron@gmail.com>
2025-03-20 18:15:48 +0200

    full task

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,15 +1,23 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Info from './Info';
+import Help from './Help';
+import Forms from './Forms';
+import Management from './Management';
 
 function App() {
-
   return (
-    <>
-    <h1></h1>
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/forms" element={<Forms />} />
+        <Route path="/management" element={<Management />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
